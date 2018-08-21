34cf8a9e54023b68e2e2af467b9be3216ad0fee1
romain <rom1@canel.ch>
2018-08-21 09:25:55 +0200

    Add ZX Spectrum style layout

--- rpiZXloader.py
+++ rpiZXloader.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from tkinter import *
+from PIL import Image, ImageTk
 import simpleaudio as sa
 
 
 app_name = "rpiZXloader"
+logo_title_path = "/home/romain/Documents/rpiZXloader/logo/header.png"
+logo_bg_path = "/home/romain/Documents/rpiZXloader/logo/logo-rainbow.png"
 
 games_library = (
     (
@@ -47,40 +50,83 @@ games_library = (
 
 start_games_pages = 1
 current_page = 1
-nb_games_per_page = 5
+nb_games_per_page = 4
+
+window_background =  "black"
+
 
 class MainWin(Frame):
     """
     """
     def __init__(self, master = None):
         Frame.__init__(self, master)
+        self.master = master
         self.master.title(app_name)
-        self.master.geometry("150x200")
-        #self.master.attribute)("-fullscreen", True)
+        screen_width  = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
+        self.master.geometry("%dx%d" % (screen_width, screen_height))
+        self.master.config(background = window_background)
+        self.master.attributes("-fullscreen", True)
 
-        self.f_but_games = Frame(master)
+        self.master.bind("<Escape>", self.prog_quit)
+
+        #  LOGO
+        ## TITLE
+        logo_title_img = Image.open(logo_title_path)
+        logo_title = ImageTk.PhotoImage(logo_title_img)
+        title = Label(image = logo_title, bg = window_background)
+        title.image = logo_title
+        #title.pack(side = TOP)
+        #title.grid(row = 0, column = 1, sticky = N)
+        title.place(x = (screen_width-logo_title_img.size[0])/2,
+                    y = 0)
+        ## BACKGROUND
+        logo_bg_img = Image.open(logo_bg_path)
+        logo_bg = ImageTk.PhotoImage(logo_bg_img)
+        bg = Label(image = logo_bg, bg = window_background)
+        bg.image = logo_bg
+        #bg.pack(side = RIGHT)
+        #bg.grid(row = 1, column = 2, sticky = SE)
+        bg.place(x = screen_width - logo_bg_img.size[0],
+                 y = screen_height - logo_bg_img.size[1])
+
+        # MAIN ZONE
+        main_zone = Frame(master, bg = window_background)
+        buttons_zone = Frame(main_zone, bg = window_background)
+        self.f_but_games = Frame(buttons_zone, background = window_background)
         self.displayFrameButtonsGames(current_page)
 
-        f_but_sel_page = Frame(master)
+        f_but_sel_page = Frame(buttons_zone)
         but_next_page = Button(f_but_sel_page,
-                               text = "->",
+                               bg = window_background,
+                               fg = "white",
+                               text = "\u21FE",
                                command = lambda:self.displayFrameButtonsGames(
                                                             current_page+1)
                               )
         but_next_page.pack(side = RIGHT)
         but_prev_page = Button(f_but_sel_page,
-                               text = "<-",
+                               background = window_background,
+                               fg = "white",
+                               text = "\u21FD",
                                command = lambda:self.displayFrameButtonsGames(
                                                             current_page-1)
                                )
         but_prev_page.pack(side = LEFT)
 
         but_help = Button(f_but_sel_page,
+                          background = window_background,
+                          fg = "white",
                           text = "HELP",
                           command = lambda:self.helpWin(master))
         but_help.pack()
 
         f_but_sel_page.pack(side = BOTTOM)
+        buttons_zone.pack()
+        #main_zone.pack()
+        #main_zone.grid(row = 1, column = 1)
+        main_zone.place(x = 0,
+                        y = logo_title_img.size[1])
 
     def displayFrameButtonsGames(self, page):
         if page > int(len(games_library)/nb_games_per_page+1):
@@ -100,6 +146,10 @@ class MainWin(Frame):
             last_game = len(games_library)
         for x in range(first_game, last_game):
             but = Button(master,
+                         activebackground = "gray",
+                         bg = window_background,
+                         fg = "white",
+                         relief = "sunken",
                          text = games_library[x][0],
                          #width = 30,
                          command = lambda:self.playWave(games_library[x][1]))
@@ -119,6 +169,9 @@ class MainWin(Frame):
         display.pack()
         button_quit = Button(pup, text = "Quit", command = pup.destroy)
         button_quit.pack()
+
+    def prog_quit(self):
+        self.master.quit()
 
 
 if __name__ == '__main__':
